d42fd47e2a7f4b399dd95e35b97f28bca06f4308
Sujan Adiga <sujanadiga1507@gmail.com>
2025-02-05 12:20:30 +0530

    third commit

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: "export",
+  basePath: "",
   images: {
     unoptimized: true,
-    domains: ["localhost"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io",
-        port: "",
-      },
-    ],
   },
 };
 
 module.exports = nextConfig;
+
