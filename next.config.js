5d6d5b14f8d972d2ce4bcfd020475744c5d4705c
Sujan Adiga <sujanadiga1507@gmail.com>
2025-02-06 16:19:57 +0530

    Updated config for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   output: "export",
-  basePath: "",
+  basePath: isProd ? "/research-society-manipal-website" : "",
+  assetPrefix: isProd ? "/research-society-manipal-website/" : "",
+  trailingSlash: true,
   images: {
     unoptimized: true,
   },
 };
 
 module.exports = nextConfig;
-
