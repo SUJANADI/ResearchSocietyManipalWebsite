f69dd9d22bc53b13a53a5b7bd86ca54a22be1e2d
Sujan Adiga <sujanadiga1507@gmail.com>
2025-02-07 18:41:39 +0530

    fixed production issues

--- next.config.js
+++ next.config.js
@@ -3,12 +3,14 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/research-society-manipal-website" : "",
-  assetPrefix: isProd ? "/research-society-manipal-website/" : "",
   trailingSlash: true,
   images: {
-    unoptimized: true,
+    unoptimized: true, 
   },
+  ...(isProd && {
+    basePath: "/ResearchSocietyManipalWebsite", 
+    assetPrefix: "/ResearchSocietyManipalWebsite/",
+  }),
 };
 
 module.exports = nextConfig;
